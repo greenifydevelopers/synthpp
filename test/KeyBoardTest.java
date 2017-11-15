843656c066a4b32b9f99a3918d03563c1f0b0f1e
NICKS-LAPTOP\Nicholas <gilman96nick@gmail.com>
2017-11-14 19:06:33 -0500

    Started tests for KeyBoard, added a getter to KeyBoard

---
+++ test/KeyBoardTest.java
@@ -0,0 +1,89 @@
+import org.junit.Before; //JUnit 4
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.Test; //JUnit 4
+
+import processing.core.PApplet;
+import synthpp.KeyBoard;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Nested
+@DisplayName("KeyBoard Testing")
+public class KeyBoardTest {
+    private static KeyBoard keyboard;
+    private static PApplet pApp;
+    @Before
+
+    public void initialize() {keyboard = new KeyBoard(pApp, 250, 100, 200,200);}
+
+    @Test
+    @DisplayName("Test Creation")
+    public void checkCreation(){
+        boolean results = false;
+        if (keyboard != null){
+            results = true;
+        }
+        assertEquals(true, results);
+    }
+
+    @Test
+    @DisplayName("Test Get Octave")
+    public void checkGetOctave(){
+        boolean results = false;
+        int currentOctave = 100;
+        currentOctave = keyboard.getOctave();
+        if (currentOctave >= -1 && currentOctave <= 11){
+            results = true;
+        }
+        assertEquals(true, results);
+    }
+
+    @Test
+    @DisplayName("Test Set Octave")
+    public void checkSetOctave(){
+        boolean results = false;
+        keyboard.setOctave(4);
+        if (keyboard.getOctave() == 4){
+            results = true;
+        }
+        assertEquals(true, results);
+    }
+
+    @Test
+    @DisplayName("Test Get Reference Tones")
+    public void checkGetReferenceTones(){
+        boolean results = true;
+        int[] refToneCopy = keyboard.getReferenceTones();
+        int prev = refToneCopy[0];
+        int current;
+        for (int x = 1; x < refToneCopy.length; x++){
+            current = refToneCopy[x];
+            if (current != prev + 1){
+                results = false;
+            }
+            prev = current;
+        }
+        assertEquals(true, results);
+    }
+
+    @Test
+    @DisplayName("Test Shift Octave")
+    public void checkShiftOctave(){
+        boolean results = true;
+        int[] refToneCopy = keyboard.getReferenceTones();
+        int[] comparisonArr =  {0,1,2,3,4,5,6,7,8,9,10,11,12};
+        keyboard.setOctave(0);
+        for (int x = 0; x < refToneCopy.length; x++){
+            if (refToneCopy[x] != comparisonArr[x]){
+                results = false;
+            }
+        }
+
+        assertEquals(true, results);
+    }
+
+
+
+
+}
